efcc8453179b8a3a5dd8d5bbe5342b47696376d1
JMeckel <Jan.Meckelholt@mercedes-benz.com>
2024-03-21 15:55:56 +0100

    testing with await (runCurrent)

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
@@ -0,0 +1,51 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.android.architecture.blueprints.todoapp.MainDispatcherRule
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+
+@ExperimentalCoroutinesApi
+class StatisticsViewModelTest {
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    // Set the main coroutines dispatcher for unit testing.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainDispatcherRule = MainDispatcherRule()
+
+    // Subject under test
+    private lateinit var statisticsViewModel: StatisticsViewModel
+
+    // Use a fake repository to be injected into the view model.
+    private lateinit var tasksRepository: FakeTestRepository
+
+    @Before
+    fun setupStatisticsViewModel() {
+        // Initialise the repository with no tasks.
+        tasksRepository = FakeTestRepository()
+
+        statisticsViewModel = StatisticsViewModel(tasksRepository)
+    }
+
+    @Test
+    fun loadTasks_loading() = runTest() {
+        launch {
+            statisticsViewModel.refresh()
+            assertTrue(statisticsViewModel.dataLoading.getOrAwaitValue())
+            runCurrent()
+            assertFalse(statisticsViewModel.dataLoading.getOrAwaitValue())
+        }
+    }
+}
